593447b8cb12d6a4e32c672fd9310ee7d92ac0b4
jose <jose.collazzi@manywho.com>
2018-09-24 10:34:00 +0100

    CORE-4684 show only description for bad request error from azure

--- src/main/java/com/manywho/services/azure/oauth/AuthResponseHandler.java
+++ src/main/java/com/manywho/services/azure/oauth/AuthResponseHandler.java
@@ -5,8 +5,8 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.util.EntityUtils;
-
 import java.io.IOException;
+import java.util.HashMap;
 
 public class AuthResponseHandler implements ResponseHandler {
 
@@ -26,8 +26,12 @@ public class AuthResponseHandler implements ResponseHandler {
         }
 
         if (status == 400) {
-            String error = EntityUtils.toString(httpResponse.getEntity());
-            throw new RuntimeException(error);
+            String errorString = EntityUtils.toString(httpResponse.getEntity());
+            HashMap error = new ObjectMapper().readValue(errorString, HashMap.class);
+
+            if (error.get("error_description") != null) {
+                throw new RuntimeException(error.get("error_description").toString());
+            }
         }
 
         throw new RuntimeException(httpResponse.getStatusLine().toString());
